f374e4d15f1fafc7e4585304f5bd6119ce5a591d
john.morris <jmo51@sbgml02764.sbg.skybettingandgaming.com>
2017-07-29 17:13:54 +0100

    added comments to library files

--- test/allTest.js
+++ test/allTest.js
@@ -44,6 +44,7 @@ describe("Main Path, Inner Boundary Values {1-3999}", function () {
     })
 })
 
+
 describe("Main Path, Check combinations {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}", function () {
     var expectedRoman, actualRoman, responseCode;
     var values = [4, 5, 9, 10, 11, 39, 40, 41, 89, 90, 91, 389, 400, 450, 899, 900, 999, 1200];
@@ -107,11 +108,12 @@ describe("Alternative Path, Check with 0", function () {
             console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
             console.log();
         }
-        assert ((expectedRoman ==actualRoman), expectedRoman + " != " + actualRoman);
+        assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
         assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
     })
 })
+
 
 describe("Alternative Path, Check with 9800", function () {
     var value = 9800;
@@ -138,11 +140,12 @@ describe("Alternative Path, Check with 9800", function () {
             console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
             console.log();
         }
-        assert ((expectedRoman ==actualRoman), expectedRoman + " != " + actualRoman);
+        assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
         assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
     })
 })
+
 
 describe("Alternative Path, Check with -3", function () {
     var value = -3;
@@ -204,6 +207,7 @@ describe("Alternative Path, Check with no values", function () {
     })
 })
 
+
 describe("Alternative Path, with chars ", function () {
     var value = "roman";
     var expectedRoman, actualRoman;
@@ -229,7 +233,7 @@ describe("Alternative Path, with chars ", function () {
             console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
             console.log();
         }
-        assert ((expectedRoman ==actualRoman), expectedRoman + " != " + actualRoman);
+        assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
         assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
     })
@@ -261,7 +265,7 @@ describe("Alternative Path, with decimal values  ", function () {
             console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
             console.log();
         }
-        assert ((expectedRoman ==actualRoman), expectedRoman + " != " + actualRoman);
+        assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
         assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
     })
