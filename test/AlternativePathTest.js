bdb5982ec4f298355470066c3563974138635d34
john.morris <jmo51@sbgml02764.sbg.skybettingandgaming.com>
2017-07-30 07:09:53 +0100

    added more negative scenarios, and also put main & alternative path scenarios in different test files

--- test/AlternativePathTest.js
+++ test/AlternativePathTest.js
@@ -1,5 +1,5 @@
 /**
- * Created by jmo51 on 29/07/2017.
+ * Created by John Morris on 29/07/2017.
  */
 
 var request = require("request");
@@ -7,80 +7,6 @@ var romanApi = require("../lib/romanApi");
 var endPoint = require("../config/env.json").endPoint;
 var debugg = require("../config/env.json").debugger;
 var assert = require('assert');
-
-describe("Main Path, Inner Boundary Values {1-3999}", function () {
-    var expectedRoman, actualRoman;
-    var values = [1, 15, 3999];
-    var expectedRomanValues = new Array();
-    for (i = 0; i < values.length; i++) {
-        expectedRomanValues[i] = romanApi.data.intToRoman(values[i]);
-    }
-    var i = 0;
-    values.forEach(function (value) {
-        it("" + value, function (done) {
-            this.timeout(550);
-            request(endPoint + value, function (error, response, body) {
-                expectedRoman = romanApi.data.intToRoman(value);
-                actualRoman = body;
-                if (debugg) {
-                    console.log('statusCode        :', response.statusCode);
-                }
-            });
-            setTimeout(done, 450);
-        });
-        it("" + values[i] + " =  " + expectedRomanValues[i], function (yes) {
-            if (debugg) {
-                console.log('expectedRoman     :', expectedRoman);
-                console.log('actualRoman       :', actualRoman);
-                console.log('expected = actual :', expectedRoman == actualRoman);
-                console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
-                console.log();
-            }
-            assert(actualRoman, expectedRoman, expectedRoman + " != " + actualRoman);
-            assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
-            setTimeout(yes, 250);
-        })
-        i++;
-    })
-})
-
-
-describe("Main Path, Check combinations {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}", function () {
-    var expectedRoman, actualRoman, responseCode;
-    var values = [4, 5, 9, 10, 11, 39, 40, 41, 89, 90, 91, 389, 400, 450, 899, 900, 999, 1200];
-    var expectedRomanValues = new Array();
-    for (i = 0; i < values.length; i++) {
-        expectedRomanValues[i] = romanApi.data.intToRoman(values[i]);
-    }
-    var i = 0;
-    values.forEach(function (value) {
-        it("" + value, function (done) {
-            this.timeout(550);
-            request(endPoint + value, function (error, response, body) {
-                expectedRoman = romanApi.data.intToRoman(value);
-                actualRoman = body;
-                responseCode = response.statusCode;
-                if (debugg) {
-                    console.log('statusCode        :', responseCode);
-                }
-            });
-            setTimeout(done, 450);
-        });
-        it("" + values[i] + " =  " + expectedRomanValues[i], function (yes) {
-            if (debugg) {
-                console.log('expectedRoman     :', expectedRoman);
-                console.log('actualRoman       :', actualRoman);
-                console.log('expected = actual :', expectedRoman == actualRoman);
-                console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
-                console.log();
-            }
-            assert(actualRoman, expectedRoman, expectedRoman + " != " + actualRoman);
-            assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
-            setTimeout(yes, 250);
-        })
-        i++;
-    })
-})
 
 
 describe("Alternative Path, Check with 0", function () {
@@ -202,7 +128,7 @@ describe("Alternative Path, Check with no values", function () {
             console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
             console.log();
         }
-        assert(false, "No value entered");
+        assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
     })
 })
@@ -268,5 +194,72 @@ describe("Alternative Path, with decimal values  ", function () {
         assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
         assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
         setTimeout(yes, 250);
+    })
+})
+
+describe("Alternative Path, with Roman Numerals  ", function () {
+    var value = "XII";
+    var expectedRoman, actualRoman;
+    expectedRoman = romanApi.data.intToRoman(value);
+    it("" + value, function (done) {
+        this.timeout(350);
+        request(endPoint + value, function (error, response, body) {
+                if (debugg) {
+                    console.log('error             :', error);
+                    console.log('statusCode        :', response.statusCode);
+                }
+                expectedRoman = romanApi.data.intToRoman(value);
+                actualRoman = body;
+            }
+        );
+        setTimeout(done, 250);
+    })
+    it("" + value + " =  " + expectedRoman, function (yes) {
+        if (debugg) {
+            console.log('expectedRoman     :', expectedRoman);
+            console.log('actualRoman       :', actualRoman);
+            console.log('expected = actual :', expectedRoman == actualRoman);
+            console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
+            console.log();
+        }
+        assert((expectedRoman == actualRoman), expectedRoman + " != " + actualRoman);
+        assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
+        setTimeout(yes, 250);
+    })
+})
+
+describe("Alternative Path, with special characters ', !, & ", function () {
+    var expectedRoman, actualRoman;
+    var values = ["'", "!", "&"];
+    var expectedRomanValues = new Array();
+    for (i = 0; i < values.length; i++) {
+        expectedRomanValues[i] = romanApi.data.intToRoman(values[i]);
+    }
+    var i = 0;
+    values.forEach(function (value) {
+        it("" + value, function (done) {
+            this.timeout(550);
+            request(endPoint + value, function (error, response, body) {
+                expectedRoman = romanApi.data.intToRoman(value);
+                actualRoman = body;
+                if (debugg) {
+                    console.log('error             :', error);
+                    console.log('statusCode        :', response.statusCode);
+                }
+            });
+            setTimeout(done, 450);
+        });
+        it("" + values[i] + " =  " + expectedRomanValues[i], function (yes) {
+            if (debugg) {
+                console.log('expectedRoman     :', expectedRoman);
+                console.log('actualRoman       :', actualRoman);
+                console.log('expected = actual :', expectedRoman == actualRoman);
+                console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
+                console.log();
+            }
+            assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
+            setTimeout(yes, 250);
+        })
+        i++;
     })
 })
---
+++ test/MainPathTest.js
@@ -0,0 +1,85 @@
+/**
+ * Created by John Morris on 29/07/2017.
+ */
+
+var request = require("request");
+var romanApi = require("../lib/romanApi");
+var endPoint = require("../config/env.json").endPoint;
+var debugg = require("../config/env.json").debugger;
+var assert = require('assert');
+
+describe("Main Path, Inner Boundary Values {1-3999}", function () {
+    var expectedRoman, actualRoman;
+    var values = [1, 15, 3999];
+    var expectedRomanValues = new Array();
+    for (i = 0; i < values.length; i++) {
+        expectedRomanValues[i] = romanApi.data.intToRoman(values[i]);
+    }
+    var i = 0;
+    values.forEach(function (value) {
+        it("" + value, function (done) {
+            this.timeout(550);
+            request(endPoint + value, function (error, response, body) {
+                expectedRoman = romanApi.data.intToRoman(value);
+                actualRoman = body;
+                if (debugg) {
+                    console.log('error             :', error);
+                    console.log('statusCode        :', response.statusCode);
+                }
+            });
+            setTimeout(done, 450);
+        });
+        it("" + values[i] + " =  " + expectedRomanValues[i], function (yes) {
+            if (debugg) {
+                console.log('expectedRoman     :', expectedRoman);
+                console.log('actualRoman       :', actualRoman);
+                console.log('expected = actual :', expectedRoman == actualRoman);
+                console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
+                console.log();
+            }
+            assert(actualRoman, expectedRoman, expectedRoman + " != " + actualRoman);
+            assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
+            setTimeout(yes, 250);
+        })
+        i++;
+    })
+})
+
+
+describe("Main Path, Check combinations {M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1}", function () {
+    var expectedRoman, actualRoman, responseCode;
+    var values = [4, 5, 9, 10, 11, 39, 40, 41, 89, 90, 91, 389, 400, 450, 899, 900, 999, 1200];
+    var expectedRomanValues = new Array();
+    for (i = 0; i < values.length; i++) {
+        expectedRomanValues[i] = romanApi.data.intToRoman(values[i]);
+    }
+    var i = 0;
+    values.forEach(function (value) {
+        it("" + value, function (done) {
+            this.timeout(550);
+            request(endPoint + value, function (error, response, body) {
+                expectedRoman = romanApi.data.intToRoman(value);
+                actualRoman = body;
+                responseCode = response.statusCode;
+                if (debugg) {
+                    console.log('error             :', error);
+                    console.log('statusCode        :', responseCode);
+                }
+            });
+            setTimeout(done, 450);
+        });
+        it("" + values[i] + " =  " + expectedRomanValues[i], function (yes) {
+            if (debugg) {
+                console.log('expectedRoman     :', expectedRoman);
+                console.log('actualRoman       :', actualRoman);
+                console.log('expected = actual :', expectedRoman == actualRoman);
+                console.log('isRoman           :', romanApi.data.patternMatch(actualRoman));
+                console.log();
+            }
+            assert(actualRoman, expectedRoman, expectedRoman + " != " + actualRoman);
+            assert(romanApi.data.patternMatch(actualRoman), "returned isn't a roman");
+            setTimeout(yes, 250);
+        })
+        i++;
+    })
+})
